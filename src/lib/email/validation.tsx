993138f8379f721e6d90853151c6667d07c849da
BautistaPessagno <bpessagno@itba.edu.ar>
2026-01-12 17:18:18 -0300

    packages and others

--- src/lib/email/validation.tsx
+++ src/lib/email/validation.tsx
@@ -1,12 +1,11 @@
-import { validate, res } from "email-validator";
+import { validate } from "email-validator";
 
 export function validateEmail(email: string): boolean {
-  validate(email); // true
-  if (res) {
+  const isValid = validate(email);
+  if (isValid) {
     console.log("Email is valid");
-    return true;
   } else {
     console.log("Email is invalid");
-    return false;
   }
+  return isValid;
 }
