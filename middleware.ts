55a1810333d9650a0a4332a2c8537463189e8ff4
BautistaPessagno <bpessagno@itba.edu.ar>
2026-01-20 16:00:01 -0300

    middleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+export { proxyHandler as middleware } from "@/src/lib/proxy";
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
